a422a181bc02dc8e56ea32a3728b74153ec89886
daisyLsbu <mymail.daisy@gmail.com>
2023-06-15 15:31:54 +0100

    test commit

---
+++ setup.sh
@@ -0,0 +1,21 @@
+# from parent directory
+
+git --version
+git config --global user.name daisyLsbu
+git config --global user.email mymail.daisy@gmail.com
+git clone <>
+# git init 
+git pull  
+git add .
+git commit -m 'test commit'
+git pull  
+git push -u origin main
+
+setup git ignore for venv
+
+python3 -m venv venv
+source venv/bin/activate
+
+python3 -m pip install --upgrade pip
+
+
