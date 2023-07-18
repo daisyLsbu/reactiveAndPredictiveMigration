5b7420c47d63082b0241a82683365dc57645e653
daisyLsbu <119698416+daisyLsbu@users.noreply.github.com>
2023-07-18 17:47:21 +0100

    Update buildsetup.sh for git path

--- scripts/buildsetup.sh
+++ scripts/buildsetup.sh
@@ -1,6 +1,6 @@
 sudo apt update
 sudo apt install git
-git clone https://github.com/daisyLsbu/WebApplicationEncryptDecrypt.git
+git clone https://github.com/daisyLsbu/TelemetryApplication.git
 cd TelemetryApplication
 sudo apt install python3
 sudo apt install venv
