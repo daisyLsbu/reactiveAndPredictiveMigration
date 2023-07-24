b775c2e87c092fdb406f3fdedde53d8747a6e90b
daisyLsbu <119698416+daisyLsbu@users.noreply.github.com>
2023-07-24 18:12:33 +0100

    Update buildsetup.sh

--- scripts/buildsetup.sh
+++ scripts/buildsetup.sh
@@ -1,7 +1,6 @@
 sudo apt update
 sudo apt install git
+git --version
 git clone https://github.com/daisyLsbu/TelemetryApplication.git
+git pull 
 cd TelemetryApplication
-sudo apt install python3
-sudo apt install venv
-sudo apt install < requirement.txt
