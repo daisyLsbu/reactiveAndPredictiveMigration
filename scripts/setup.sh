1e8aba12e00a79e416cda9041ef193cbe4666633
daisyLsbu <119698416+daisyLsbu@users.noreply.github.com>
2023-07-24 18:18:41 +0100

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,6 +3,6 @@ sudo apt-get install python3-venv
 python3 -m venv venv
 source venv/bin/activate
 python3 -m pip install --upgrade pip
-pip install -r requirements.txt
+pip install -r requirement.txt
 
 
