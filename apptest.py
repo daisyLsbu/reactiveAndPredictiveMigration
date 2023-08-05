76f6ab4a15fe5185b6878497e456f5bd30d919f1
daisyLsbu <mymail.daisy@gmail.com>
2023-08-05 09:52:48 +0100

    combineDatawithdockerstat

--- apptest.py
+++ apptest.py
@@ -1,33 +1,21 @@
-from flask import Flask
 import psutilThread 
 import dockerStat
 import sys
 
-app = Flask(__name__)
-
-welcomeText = 'welcome to Home page'
-port_num = 5002
-
-@app.route('/')
-def defaultpath():
-   return welcomeText 
-
-@app.get("/devicedetails")
 def get_deviceDetails():
    result = psutilThread.startThreads()
    return result
 
-@app.get("/containers")
 def get_containersDetails():
    result = dockerStat.dockerstatinfo()
    return result
 
-@app.get("/combined")
 def get_combineddata():
     result = psutilThread.startThreads()
     result['containers'] = dockerStat.dockerstatinfo()
     return result
 
 if __name__ == '__main__':
-   #port_num = int(sys.argv[1])
-   app.run(host="0.0.0.0", port=port_num)
+   #print(get_deviceDetails())
+   #print(get_containersDetails())
+   print(get_combineddata())
--- dockerStat.py
+++ dockerStat.py
@@ -4,6 +4,8 @@ from pprint import PrettyPrinter
 def dockerstatinfo():
     pp = PrettyPrinter(indent=2)
     client = docker.from_env()
+    result = []
+    result_out = {}
 
     for containers in client.containers.list():
         containerid = containers.short_id
@@ -12,8 +14,15 @@ def dockerstatinfo():
 
 
         # from informations : UsageDelta = 25382985593 - 25382168431
+        '''
         pp.pprint(containerid)
         pp.pprint(stats)
         pp.pprint(UsageDelta)
+        '''
+        result.append(stats)
+    return result
 
-        return stats
+'''
+result_out = dockerstatinfo()
+print(result_out)
+'''
