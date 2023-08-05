e9eeea3233677c6467530285745bdae9945f1c98
daisyLsbu <mymail.daisy@gmail.com>
2023-08-05 06:52:43 +0100

    dockerstatAPI

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask
 import psutilThread 
+import dockerStat
 import sys
 
 app = Flask(__name__)
 
 welcomeText = 'welcome to Home page'
-port_num = 5001
+port_num = 5002
 
 @app.route('/')
 def defaultpath():
@@ -16,6 +17,11 @@ def get_deviceDetails():
    result = psutilThread.startThreads()
    return result
 
+@app.get("/containers")
+def get_containersDetails():
+   result = dockerStat.dockerstatinfo()
+   return result
+
 if __name__ == '__main__':
-   port_num = int(sys.argv[1])
+   #port_num = int(sys.argv[1])
    app.run(host="0.0.0.0", port=port_num)
---
+++ dockerStat.py
@@ -0,0 +1,19 @@
+import docker
+from pprint import PrettyPrinter
+
+def dockerstatinfo():
+    pp = PrettyPrinter(indent=2)
+    client = docker.from_env()
+
+    for containers in client.containers.list():
+        containerid = containers.short_id
+        stats = containers.stats(decode=None, stream = False)
+        UsageDelta = stats['cpu_stats']['cpu_usage']['total_usage'] - stats['precpu_stats']['cpu_usage']['total_usage']
+
+
+        # from informations : UsageDelta = 25382985593 - 25382168431
+        pp.pprint(containerid)
+        pp.pprint(stats)
+        pp.pprint(UsageDelta)
+
+        return stats
