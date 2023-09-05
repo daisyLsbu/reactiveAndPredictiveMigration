5fd5d97c5c777ba740bc809aabb88bf6be5cc6ec
daisyLsbu <mymail.daisy@gmail.com>
2023-09-05 16:16:24 +0100

    rttAsApi

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 import psutilThread 
 import dockerStat
+import rtt
 import sys
 
 app = Flask(__name__)
@@ -28,6 +29,61 @@ def get_combineddata():
     result['containers'] = dockerStat.dockerstatinfo()
     return result
 
+@app.get("/rtt")
+def get_rttdata():
+   hosts = [
+    '8.8.8.8'
+      ]
+   result = rtt.ping_host(hosts)
+   return result
+
+@app.route('/greet', methods=['GET'])
+def greet():
+    name = request.args.get('name')  # Get the 'name' parameter from the query string
+    if name:
+        return jsonify({'message': f'Hello, {name}!'})
+    else:
+        return jsonify({'error': 'Name parameter is missing'}), 400
+
+
+
+@app.route('/process-json', methods=['POST'])
+def process_json():
+    try:
+        # Get JSON data from the request
+        data = request.get_json()
+
+        # Check if the JSON data is valid
+        if data is None:
+            return jsonify({'error': 'Invalid JSON data'}), 400
+
+        # Process the JSON data here (you can customize this part)
+        result = {'message': 'JSON data received successfully', 'data': data}
+
+        return jsonify(result)
+
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+@app.route('/rttData', methods=['POST'])
+def rttData_json():
+    try:
+        # Get JSON data from the request
+        data = request.get_json()
+
+        # Check if the JSON data is valid
+        if data is None:
+            return jsonify({'error': 'Invalid JSON data'}), 400
+
+        # Process the JSON data here (you can customize this part)
+        hosts = data["hosts"]
+        result = rtt.ping_host(hosts)
+        return jsonify(result)
+
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
 if __name__ == '__main__':
    #port_num = int(sys.argv[1])
    app.run(host="0.0.0.0", port=port_num)
+   #get_rttdata()
--- rtt.py
+++ rtt.py
@@ -5,6 +5,26 @@ from pythonping import ping
 hosts = [
     '8.8.8.8'
 ]
+rtt_values = []
+
+
+def ping_host(hosts):
+    for host in hosts:
+        print("Pinging %s" %(host))
+        ping_result = ping(target=host, size=40, count=10) #ping(target=host, count=10000, size=1500) #
+        rtt_values.append(
+        {
+        'host': host,
+        'avg_latency': ping_result.rtt_avg_ms,
+        'min_latency': ping_result.rtt_min_ms,
+        'max_latency': ping_result.rtt_max_ms,
+        'packet_loss': ping_result.packet_loss
+        })
+    return rtt_values
+
+#print(ping_host(hosts))
+
+'''
 #worked
 #ping('8.8.8.8') 
 #[ping(host) for host in hosts]
@@ -12,22 +32,6 @@ hosts = [
 #for host in hosts:
 #    ping_result = ping(host)
 #    print(ping_result.rtt_avg)
-
-def ping_host(host):
-    ping_result = ping(target=host, size=40, count=10) #ping(target=host, count=10000, size=1500) #
-
-    return {
-        'host': host,
-        'avg_latency': ping_result.rtt_avg_ms,
-        'min_latency': ping_result.rtt_min_ms,
-        'max_latency': ping_result.rtt_max_ms,
-        'packet_loss': ping_result.packet_loss
-    }
-
-[print(ping_host(host)) for host in hosts]
-
-'''
-
 
 def ping_host(host):
     ping_result = ping(target=host, count=10, timeout=2)
