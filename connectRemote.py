ce9b49ae1165ca0a45efc8d1072584d07059b8f0
daisyLsbu <mymail.daisy@gmail.com>
2023-09-29 17:55:52 +0100

    scp-working

--- connectRemote.py
+++ connectRemote.py
@@ -1,4 +1,5 @@
 import paramiko
+from scp import SCPClient
 
 def sshTest():
     ssh = paramiko.SSHClient()
@@ -21,10 +22,35 @@ def sshrestore(destIp):
     ssh.connect(destIp, 22, 'ubuntu', 'ubuntu')
     ssh.exec_command('python3 restoreimage.py')
 
-#sshTest()
-#docker run -d 5a81c4b8502e
-#sshmigrate() 
-#sshrestore()
+def createSSHClient(server, port, user, password):
+    client = paramiko.SSHClient()
+    client.load_system_host_keys()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    client.connect(server, port, user, password)
+    return client
+
+def sshcopy_(server1, server2, port, user, password):
+    ssh = createSSHClient(server1, port, user, password)
+    scp = SCPClient(ssh.get_transport())
+    scp.get('ubuntu-test.img')
+    ssh = createSSHClient(server2, port, user, password)
+    scp = SCPClient(ssh.get_transport())
+    scp.put('ubuntu-test.img')
+
+def sshcopy(server1, server2, port, user, password):
+    ssh = createSSHClient(server1, port, user, password)
+    scp = SCPClient(ssh.get_transport())
+    scp.get('/home/ubuntu/images/ubuntu-test.img')
+    print("got")
+
+    ssh = createSSHClient(server2, port, user, password)
+    scp = SCPClient(ssh.get_transport())
+    scp.put('ubuntu-test.img')
+    ssh.exec_command('sudo mv ubuntu-test.img /home/ubuntu/images/')
+
+if __name__ == '__main__':
+    print("sshcopy")
+    sshcopy('192.168.122.80', '192.168.122.210', 22, 'ubuntu', 'ubuntu')
 
 
 
