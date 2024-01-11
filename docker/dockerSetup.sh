a04423be6ef7cd7bb559df442dea0b385bf64f06
daisyLsbu <mymail.daisy@gmail.com>
2024-01-11 18:29:00 +0000

    cleancode

--- docker/dockerSetup.sh
+++ docker/dockerSetup.sh
@@ -1,3 +1,5 @@
+# this file can help setup docker and start the containers after downloading the prebuild container with existing stress python file to randomly stress the container.
+
 #install docker
 #add user to group
 #docker login
@@ -5,6 +7,6 @@
 #run container with volume mounting
 sudo docker run -d 86a171db06e1 python3 looprandomstress.py
 #copy migration script outside the folder
-cp ../migrateVictim.py '/home/ubuntu/.'
-cp ../transferimage.py '/home/ubuntu/.'
-cp ../restoreimage.py '/home/ubuntu/.'
+cp migrateVictim.py '/home/ubuntu/.'
+cp transferimage.py '/home/ubuntu/.'
+cp restoreimage.py '/home/ubuntu/.'
