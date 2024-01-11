aef6cedff6fb3f7fe8b1ef0d6a94080595b31c71
daisyLsbu <mymail.daisy@gmail.com>
2024-01-11 20:25:33 +0000

    cleanup

--- migrationAgent.py
+++ migrationAgent.py
@@ -2,7 +2,7 @@ import influxdb_client, os, time
 from influxdb_client import InfluxDBClient, Point, WritePrecision
 from influxdb_client.client.write_api import SYNCHRONOUS
 from pprint import PrettyPrinter
-from collectDisplay import getCurrentData
+from images.collectDisplay import getCurrentData
 import asyncio
 import connectRemote
 
